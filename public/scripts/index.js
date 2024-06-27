fd9f6c2fbd4aa25c65d096892179f563fa1a4b6f
Fabio Marques <engfabiom@gmail.com>
2024-06-27 16:41:42 +0900

    include of backgound art image and some decorations on articles for better display

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,13 +4,21 @@ window.addEventListener("scroll", (event) => {
   let arts = document.querySelectorAll("#arts article");
   let artsNavGallery = document.querySelectorAll("#nav-gallery li");
   let scroll = this.scrollY;
+
   for (art of Array.from(arts).reverse()) {
-    if (scroll+1 >= art.offsetTop) {
+    if (scroll + 1 >= art.offsetTop) {
       break;
     }
     art = "";
   }
-  artsNavGallery.forEach(nav => {
-    nav.matches("."+art.id)? nav.classList.add("highlight"):nav.classList.remove("highlight") ;
-  })
+  artsNavGallery.forEach((nav) => {
+    nav.matches("." + art.id)
+      ? nav.classList.add("highlight")
+      : nav.classList.remove("highlight");
+  });
+
+  let bgiElement = document.querySelector("#background-image-container");
+  let backgroundImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
+  backgroundImage = art !== "" ? backgroundImage : "";
+  bgiElement.style.backgroundImage = backgroundImage;
 });

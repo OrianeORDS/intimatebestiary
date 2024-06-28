f259025a293e5624c8524178f9b7f7de6bd6bb58
sandesnp <sthasandesnp@gmail.com>
2024-06-28 22:36:37 +1000

    add declaration for art and used && operator for simplification

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,25 @@
-document.querySelector("footer p").append(` ${new Date().getFullYear()}`);
+document.querySelector('footer p').append(` ${new Date().getFullYear()}`);
 
-window.addEventListener("scroll", (event) => {
-  let arts = document.querySelectorAll("#arts article");
-  let artsNavGallery = document.querySelectorAll("#nav-gallery li");
+window.addEventListener('scroll', (event) => {
+  let arts = document.querySelectorAll('#arts article');
+  let artsNavGallery = document.querySelectorAll('#nav-gallery li');
   let scroll = this.scrollY;
-
+  let art;
   for (art of Array.from(arts).reverse()) {
     if (scroll + 1 >= art.offsetTop) {
       break;
     }
-    art = "";
+    art = '';
   }
   artsNavGallery.forEach((nav) => {
-    nav.matches("." + art.id)
-      ? nav.classList.add("highlight")
-      : nav.classList.remove("highlight");
+    nav.matches('.' + art.id)
+      ? nav.classList.add('highlight')
+      : nav.classList.remove('highlight');
   });
 
-  let bgiElement = document.querySelector("#background-image-container");
+  let bgiElement = document.querySelector('#background-image-container');
   let backgroundImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
-  backgroundImage = art !== "" ? backgroundImage : "";
+  // backgroundImage = art !== '' ? backgroundImage : '';
+  backgroundImage = art && backgroundImage;
   bgiElement.style.backgroundImage = backgroundImage;
 });

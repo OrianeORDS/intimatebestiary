c8b7e7c36c2c3f58ea36827a6fd19d0298d3b91d
Fabio Marques <engfabiom@gmail.com>
2024-06-28 22:51:24 +0900

    add smooth fade on background images

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -18,20 +18,42 @@ window.addEventListener("scroll", (event) => {
   });
 
   let bgiElement = document.querySelector("#background-image-container");
-  let backgroundImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
-  backgroundImage = art !== "" ? backgroundImage : "";
-  bgiElement.style.backgroundImage = backgroundImage;
+  let currentImage = window
+    .getComputedStyle(bgiElement)
+    .getPropertyValue("background-image");
+  let newImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
+  newImage = art !== "" ? newImage : "";
+
+  console.log(currentImage);
+  console.log(newImage.replace(/url\('/, "").replace(/'\)/, ""));
+
+  if (
+    !currentImage.includes(newImage.replace(/url\('/, "").replace(/'\)/, "")) ||
+    art === ""
+  ) {
+    bgiElement.classList.add("hidden");
+    window.setTimeout(() => {
+      bgiElement.style.backgroundImage = newImage;
+      bgiElement.classList.remove("hidden");
+      bgiElement.classList.add("visible");
+    }, 250);
+    window.setTimeout(() => {
+      bgiElement.classList.remove("visible");
+    }, 250);
+  }
 });
 
 //stop other playing audios
-document.querySelectorAll("#arts audio").forEach((audioQueried) => { 
+document.querySelectorAll("#arts audio").forEach((audioQueried) => {
   audioQueried.addEventListener("play", (event) => {
     let audioPlayed = event.target;
-    document.querySelectorAll("#arts audio").forEach((audioQueriedOnListener) => {
-      if (audioQueriedOnListener !== audioPlayed) {
-        audioQueriedOnListener.pause();
-        audioQueriedOnListener.currentTime = 0;
-      }
-    });
+    document
+      .querySelectorAll("#arts audio")
+      .forEach((audioQueriedOnListener) => {
+        if (audioQueriedOnListener !== audioPlayed) {
+          audioQueriedOnListener.pause();
+          audioQueriedOnListener.currentTime = 0;
+        }
+      });
   });
 });

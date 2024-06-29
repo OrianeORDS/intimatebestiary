25f6a7e1d8a95643ba6136351e454b1c7b8ac191
Fabio Marques <engfabiom@gmail.com>
2024-06-29 16:21:01 +0900

    fix on image distortion, include cross-fade on background image, +other minor adjustments on main.css

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,6 +5,7 @@ window.addEventListener("scroll", (event) => {
   let artsNavGallery = document.querySelectorAll("#nav-gallery li");
   let scroll = this.scrollY;
 
+  let art;
   for (art of Array.from(arts).reverse()) {
     if (scroll + 1 >= art.offsetTop) {
       break;
@@ -18,9 +19,29 @@ window.addEventListener("scroll", (event) => {
   });
 
   let bgiElement = document.querySelector("#background-image-container");
-  let backgroundImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
-  backgroundImage = art !== "" ? backgroundImage : "";
-  bgiElement.style.backgroundImage = backgroundImage;
+  let currentImage = window
+    .getComputedStyle(bgiElement)
+    .getPropertyValue("background-image");
+  let newImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
+  newImage = art && newImage;
+
+  console.log(currentImage);
+  console.log(newImage.replace(/url\('/, "").replace(/'\)/, ""));
+
+  if (
+    !currentImage.includes(newImage.replace(/url\('/, "").replace(/'\)/, "")) ||
+    art === ""
+  ) {
+    bgiElement.classList.add("hidden");
+    window.setTimeout(() => {
+      bgiElement.style.backgroundImage = newImage;
+      bgiElement.classList.remove("hidden");
+      bgiElement.classList.add("visible");
+    }, 250);
+    window.setTimeout(() => {
+      bgiElement.classList.remove("visible");
+    }, 250);
+  }
 });
 
 //stop other playing audios

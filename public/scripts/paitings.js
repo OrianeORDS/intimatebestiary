3b433eae93f1cb3d2de4bb4391fa68b43e286e0f
Fabio Marques <engfabiom@gmail.com>
2024-07-09 14:07:22 +0900

    painting page new layout adjustment (html + css + js)

--- public/scripts/paitings.js
+++ public/scripts/paitings.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", (event) => {
-  let arts = document.querySelectorAll("#arts article");
+  let paintings = document.querySelectorAll("#paintings article");
   let artsNavGallery = document.querySelectorAll("#nav-gallery li");
   let scroll = this.scrollY;
 
   let art;
-  for (art of Array.from(arts).reverse()) {
+  for (art of Array.from(paintings).reverse()) {
     if (scroll + 1 >= art.offsetTop) {
       break;
     }
@@ -15,32 +15,6 @@ window.addEventListener("scroll", (event) => {
       ? nav.classList.add("highlight")
       : nav.classList.remove("highlight");
   });
-
-  let bgiElement = document.querySelector("#background-image-container");
-  let currentImage = window
-    .getComputedStyle(bgiElement)
-    .getPropertyValue("background-image");
-  let newImage = `url('public/images/arts/paintings/${art.id}_lr.jpg')`;
-  // newImage = art !== "" ? newImage : "";
-  newImage = art && newImage;
-
-  console.log(currentImage);
-  console.log(newImage.replace(/url\('/, "").replace(/'\)/, ""));
-
-  if (
-    !currentImage.includes(newImage.replace(/url\('/, "").replace(/'\)/, "")) ||
-    art === ""
-  ) {
-    bgiElement.classList.add("hidden");
-    window.setTimeout(() => {
-      bgiElement.style.backgroundImage = newImage;
-      bgiElement.classList.remove("hidden");
-      bgiElement.classList.add("visible");
-    }, 250);
-    window.setTimeout(() => {
-      bgiElement.classList.remove("visible");
-    }, 250);
-  }
 });
 
 //stop other playing audios

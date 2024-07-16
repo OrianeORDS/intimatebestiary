3898099d3af9a08f82aa798474442f004f75b4ce
Fabio Marques <engfabiom@gmail.com>
2024-07-17 03:42:37 +0900

    new feature carousel bullets click

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -14,8 +14,10 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
     carousel.querySelector(".carousel-dots").appendChild(newDot);
   });
   if (index.current > -1) {
-    carousel.addEventListener("click", (e) => {
-      carousel.querySelectorAll(".carousel-dots div").forEach(d => d.classList.remove('carousel-dots-active'));
+    carousel.addEventListener("click", async (e) => {
+      carousel
+        .querySelectorAll(".carousel-dots div")
+        .forEach((d) => d.classList.remove("carousel-dots-active"));
       if (e.target.classList.contains("carousel-previous"))
         index.current = index.previous;
       if (e.target.classList.contains("carousel-next"))
@@ -26,15 +28,29 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
           img.classList.add("carousel-previous");
         } else if (i === index.current) {
           img.classList.add("carousel-current");
-          carousel.querySelector(".carousel-dots div:nth-child("+(i+1)+")").classList.add('carousel-dots-active');
+          carousel
+            .querySelector(".carousel-dots div:nth-child(" + (i + 1) + ")")
+            .classList.add("carousel-dots-active");
         } else if (i === index.next) {
           img.classList.add("carousel-next");
         } else {
           img.classList.add("carousel-hidden");
         }
       });
-      if ( Array.from(document.querySelectorAll(".carousel-dots div")).includes(e.target) ) {
-        //match nth element to the index
+      if (
+        Array.from(document.querySelectorAll(".carousel-dots div")).includes(
+          e.target
+        )
+      ) {
+        var dotIndex = Array.from(
+          document.querySelectorAll(".carousel-dots div")
+        ).indexOf(e.target);
+        while (index.current !== dotIndex) {
+          var next = document.querySelector(".carousel-next");
+          var previous = document.querySelector(".carousel-previous");
+          (dotIndex - index.current) > 0 ? next.click() : previous.click();
+          await new Promise(resolve => setTimeout(resolve, 333))
+        }
       }
     });
     carousel.click();

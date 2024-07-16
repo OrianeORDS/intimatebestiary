4a57cb918105d0865a8340e11f65a0ed4895540e
Fabio Marques <engfabiom@gmail.com>
2024-07-16 17:03:48 +0900

    initial join all documents. all texts to the main index page. + initial adjusts on css and js.

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -2,25 +2,33 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   var imgs = carousel.querySelectorAll("ul li img");
   let index = {
     current: imgs.length > 0 ? 0 : -1,
-    get next() { return (this.current + 1) % imgs.length; },
-    get previous() { return (this.current - 1 + imgs.length) % imgs.length; },
+    get next() {
+      return (this.current + 1) % imgs.length;
+    },
+    get previous() {
+      return (this.current - 1 + imgs.length) % imgs.length;
+    },
   };
+  imgs.forEach((img, i) => {
+    let newDot = document.createElement("div");
+    carousel.querySelector(".carousel-dots").appendChild(newDot);
+  });
   if (index.current > -1) {
     carousel.addEventListener("click", (e) => {
-      if (e.target.classList.contains("carousel-prev"))
+      if (e.target.classList.contains("carousel-previous"))
         index.current = index.previous;
       if (e.target.classList.contains("carousel-next"))
         index.current = index.next;
       imgs.forEach((img, i) => {
-        img.removeAttribute('class');
+        img.removeAttribute("class");
         if (i === index.previous) {
-          img.classList.add("side-left");
-        } else if (i === index.current){
-          img.classList.add("current");
-        } else if (i === index.next){
-          img.classList.add("side-right");
+          img.classList.add("carousel-previous");
+        } else if (i === index.current) {
+          img.classList.add("carousel-current");
+        } else if (i === index.next) {
+          img.classList.add("carousel-next");
         } else {
-          img.classList.add("hidden");
+          img.classList.add("carousel-hidden");
         }
       });
     });

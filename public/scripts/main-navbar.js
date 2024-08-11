1a8f5c4e90ad0f139be41fc77676d3a8180698fb
OrianeORDS <oriane.royon@gmail.com>
2024-08-11 12:29:36 +0200

    changes mostly on the nav bar appearance and behaviour * about the global document :     - change css from h1 et h2 : size and letter spacing     - change the content of some titles in index.html * change the main-navbar.css coding :   - nav button => change color + sizing of icon + icon:before and icon:after   - nav button => put all the code about that together, Under a specific comment   - switch the navigation__background properties to the navigatio__nav to simplify the lecture of the code   - navigation__nav => change the display to none when button unchecked   - navigation_list => change the color and effect   - take of the html and header that surrounded the whole block of coding, according to the bem methodologie

--- public/scripts/main-navbar.js
+++ public/scripts/main-navbar.js
@@ -4,10 +4,8 @@ const navNav = document.querySelector('.navigation__nav');
 
 navButton.addEventListener('change', function () {
   if (this.checked) {
-    navBackground.classList.add('navigation__background--transition');
     navNav.classList.add('navigation__nav--transition');
   } else {
-    navBackground.classList.remove('navigation__background--transition');
     navNav.classList.remove('navigation__nav--transition');
   }
 });

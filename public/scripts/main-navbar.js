dbba01ca167012002258386e8278f005d368960f
OrianeORDS <oriane.royon@gmail.com>
2024-08-13 11:21:44 +0200

    try to improve the animations on the navbar but not working : - either the js forEach navLink - nor the css @keyframes need to continue working on it another time

--- public/scripts/main-navbar.js
+++ public/scripts/main-navbar.js
@@ -1,6 +1,5 @@
-const navButton = document.querySelector('.navigation__checkbox');
-const navBackground = document.querySelector('.navigation__background');
-const navNav = document.querySelector('.navigation__nav');
+let navButton = document.querySelector('.navigation__checkbox');
+let navLink = document.querySelector('.navigation__link');
 
 navButton.addEventListener('change', function () {
   if (this.checked) {
@@ -9,3 +8,19 @@ navButton.addEventListener('change', function () {
     navNav.classList.remove('navigation__nav--transition');
   }
 });
+
+//  Try to add an eventlistener to each link in order to enlight them ... but not working 
+navLink.forEach(linkofnav => {
+  linkofnav.addEventListener("mouseover", enlighten(e) ) }); 
+  
+  function enlighten(event){
+    console.log(event)
+  }
+
+  /*
+function enlighten(event) {
+    event.srcElement.classList.add('navigation__link--enlighten');
+    setTimeout(function () {
+      event.srcElement.classList.remove("navigation__link--enlighten");}, 300);
+      console.log(event); 
+  }*/

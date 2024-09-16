96187f8f098610dfc81fb8de05300bbc3362321f
sandesnp <averagenp@gmail.com>
2024-09-16 14:03:19 +1000

    production2

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,13 @@
-document.querySelector("footer p").append(` ${new Date().getFullYear()}`);
+document.querySelector('footer p').append(` ${new Date().getFullYear()}`);
 
 var ioFooter = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    document.querySelectorAll(".footer-giraffe__img").forEach((giraffe) => {
-      giraffe.classList.toggle("hide-giraffe", !entry.isIntersecting);
-      giraffe.classList.toggle("show-giraffe", entry.isIntersecting);
+    document.querySelectorAll('.footer-giraffe__img').forEach((giraffe) => {
+      giraffe.classList.toggle('hide-giraffe', !entry.isIntersecting);
+      giraffe.classList.toggle('show-giraffe', entry.isIntersecting);
     });
   });
 });
-ioFooter.observe(document.querySelector("footer"));
+ioFooter.observe(document.querySelector('footer'));
+
+console.log('production2');

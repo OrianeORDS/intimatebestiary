ae8f139a54c2d1978e402e722a372b032ba203af
Fabio Marques <engfabiom@gmail.com>
2024-09-17 19:38:46 +0900

    background load high resolution art image + down sample the _lr images

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,20 @@
-document.querySelector('footer p').append(` ${new Date().getFullYear()}`);
+document.querySelector("footer p").append(` ${new Date().getFullYear()}`);
 
 var ioFooter = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    document.querySelectorAll('.footer-giraffe__img').forEach((giraffe) => {
-      giraffe.classList.toggle('hide-giraffe', !entry.isIntersecting);
-      giraffe.classList.toggle('show-giraffe', entry.isIntersecting);
+    document.querySelectorAll(".footer-giraffe__img").forEach((giraffe) => {
+      giraffe.classList.toggle("hide-giraffe", !entry.isIntersecting);
+      giraffe.classList.toggle("show-giraffe", entry.isIntersecting);
     });
   });
 });
-ioFooter.observe(document.querySelector('footer'));
+ioFooter.observe(document.querySelector("footer"));
+
+document.querySelectorAll("#paintings__container img").forEach(async (_img) => {
+  let newImg = document.createElement("img");
+  newImg.src = _img.src.replace(/_lr.jpg/, "_hr.jpg");
+  await newImg.addEventListener("load", () => {
+    _img.replaceWith(newImg);
+    console.log(new Date().toLocaleString(), ": ", _img);
+  });
+});
